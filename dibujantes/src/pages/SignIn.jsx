11ef3dd20929bf6e313cde246cb1cc1abbc676b6
JuanJSaavedra52 <124198434+JuanJSaavedra52@users.noreply.github.com>
2023-02-27 16:21:01 +0000

    Falta ver que se aplique la confirmación de contraseña

--- dibujantes/src/pages/SignIn.jsx
+++ dibujantes/src/pages/SignIn.jsx
@@ -1,23 +1,11 @@
+import { CompSignIn } from "../componentes/CompSignIn"
+
 export const SignIn = () => {
     return(
         <>
         <div>
             <h1>Por favor llene el siguiente formulario</h1>
-            <div>
-                <input type="text" placeholder="Nombre de usuario" />
-            </div>
-            <div>
-                <input type="password" placeholder="Correo electronico" />
-            </div>
-            <div>
-                <input type="password" placeholder="Contraseña" />
-            </div>
-            <div>
-                <input type="password" placeholder="Confirmar Contraseña" />
-            </div>
-            <div>
-                <button>Sign In</button>
-            </div>
+            <CompSignIn />
         </div>
         </>
     )
