990648bf8e8d0c95066f311457c9e0279d6748f6
HexDan <daniel.hernandez_val@uao.edu.co>
2023-04-07 11:02:15 -0500

    modified:   dibujantes/src/App.js

--- dibujantes/src/App.js
+++ dibujantes/src/App.js
@@ -1,11 +1,23 @@
-import { Login } from './pages/index/Login';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Login, SignIn, Home } from "./pages";
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Login />
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<SignIn />} />
+      <Route path="/home" element={<Home />} />
+      
+      <Route path="/home/search" element={<Search />} />
+      <Route path="/home/imagen" element={<Imagen />} />
+      <Route path="/profile" element={<Perfil />} />
+      <Route path="/profile/edit" element={<EditarPerfil />} />
+      <Route path="/subir" element={<Subir />} />
+      <Route path="/notificaciones" element={<Notificaciones />} />
+      <Route path="/mensajes" element={<Mensajes />} />
+    </Routes>
   );
-}
+};
 
 export default App;
